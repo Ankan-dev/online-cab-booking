e90a3aa79a3edd7d384c664f444e699865c6c288
Ankan Mandal <ankanmandal2001@gmail.com>
2025-01-14 16:47:50 +0530

    navigation and interlinking of pages completed

--- frontend/src/Pages/CaptainLogin.jsx
+++ frontend/src/Pages/CaptainLogin.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
 import bg from '../assets/captainCabBg.jpeg'
+import { Link } from 'react-router-dom';
 
 const CaptainLogin = () => {
+
+  const [email, setemail] = useState('');
+  const [password, setpassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data={
+      email:email,
+      password:password
+    }
+
+    console.log(data);
+    setemail('');
+    setpassword('');
+  }
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-[#2c2c36] text-white">
     <div className="flex items-center justify-center w-full h-[100vh] rounded-[10px] bg-cover bg-no-repeat" style={{ backgroundImage: `url(${bg})` }}>
@@ -12,7 +29,8 @@ const CaptainLogin = () => {
             <input
               type="email"
               placeholder="email"
-              required=""
+              value={email}
+              onChange={(e)=>setemail(e.target.value)}
               className="w-full h-[50px] bg-transparent  outline-none border-[2px] border-[rgba(255,255,255,0.2)] rounded-[40px] text-[16px] text-white px-[20px] pr-[45px]"
             />
             <i className="bx bxs-user absolute right-[20px] top-1/2 transform -translate-y-1/2"></i>
@@ -21,13 +39,14 @@ const CaptainLogin = () => {
             <input
               type="password"
               placeholder="Password"
-              required=""
+              value={password}
+              onChange={(e)=>setpassword(e.target.value)}
               className="w-full h-[50px] bg-transparent  outline-none border-[2px] border-[rgba(255,255,255,0.2)] rounded-[40px] text-[16px] text-white px-[20px] pr-[45px]"
             />
             <i className="bx bxs-lock-alt absolute right-[20px] top-1/2 transform -translate-y-1/2"></i>
           </div>
           <br />
-          <button type="submit" className="w-full h-[45px] bg-white border-none outline-none mt-[20px] rounded-[40px] shadow-[0_0_10px_rgba(0,0,0,0.1)] cursor-pointer text-[16px] text-[#333] font-semibold hover:bg-[#ff76ba] hover:text-white">
+          <button type="submit" onClick={handleSubmit} className="w-full h-[45px] bg-white border-none outline-none mt-[20px] rounded-[40px] shadow-[0_0_10px_rgba(0,0,0,0.1)] cursor-pointer text-[16px] text-[#333] font-semibold hover:bg-[#fdc80c] ">
             Login
           </button>
           <div className="text-center text-[13px] mt-[20px]">
@@ -35,7 +54,7 @@ const CaptainLogin = () => {
           </div>
           <div className="text-center text-[14.5px] my-[20px]">
             <p>
-              Don't have an account? <a href="#" className="text-white font-semibold hover:underline">Register</a>
+              Don't have an account? <Link to='/captain-signup' className="text-white font-semibold hover:underline">Register</Link>
             </p>
           </div>
         </form>
--- frontend/src/Pages/CaptainSignUp.jsx
+++ frontend/src/Pages/CaptainSignUp.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 import bg from '../assets/captainCabBg.jpeg'
+import { Link,useNavigate } from 'react-router-dom';
 
 const CaptainSignUp = () => {
+
+  const [fullname, setfullname] = useState('');
+    const [email, setemail] = useState('');
+    const [phone, setphone] = useState('');
+    const [password, setpassword] = useState('');
+    const navigate=useNavigate();
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const data={
+        email:email,
+        name:fullname,
+        phone:phone,
+        password:password
+      }
+  
+      console.log(data);
+      setfullname('');
+      setemail('');
+      setphone('');
+      setpassword('');
+    }
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-[#2c2c36] text-white">
     <div className="flex items-center justify-center w-full h-[100vh] rounded-[10px] bg-cover bg-no-repeat" style={{ backgroundImage: `url(${bg})` }}>
@@ -12,7 +36,8 @@ const CaptainSignUp = () => {
             <input
               type="text"
               placeholder="Full Name"
-              required=""
+              value={fullname}
+              onChange={(e)=>setfullname(e.target.value)}
               className="w-full h-[50px] bg-transparent  border-[rgba(255,255,255,0.2)] outline-none border-[2px]  rounded-[40px] text-[16px] text-white px-[20px] pr-[45px]"
             />
             <i className="bx bxs-user absolute right-[20px] top-1/2 transform -translate-y-1/2"></i>
@@ -21,7 +46,8 @@ const CaptainSignUp = () => {
             <input
               type="email"
               placeholder="Email"
-              required=""
+              value={email}
+              onChange={(e)=>setemail(e.target.value)}
               className="w-full h-[50px] bg-transparent  outline-none border-[2px] border-[rgba(255,255,255,0.2)] rounded-[40px] text-[16px] text-white px-[20px] pr-[45px]"
             />
             <i className="bx bxs-envelope absolute right-[20px] top-1/2 transform -translate-y-1/2"></i>
@@ -30,7 +56,8 @@ const CaptainSignUp = () => {
             <input
               type="tel"
               placeholder="Phone"
-              required=""
+              value={phone}
+              onChange={(e)=>setphone(e.target.value)}
               className="w-full h-[50px] bg-transparent outline-none border-[2px] border-[rgba(255,255,255,0.2)] rounded-[40px] text-[16px] text-white px-[20px] pr-[45px]"
             />
             <i className="bx bxs-phone absolute right-[20px] top-1/2 transform -translate-y-1/2"></i>
@@ -39,20 +66,21 @@ const CaptainSignUp = () => {
             <input
               type="password"
               placeholder="Password"
-              required=""
+              value={password}
+              onChange={(e)=>setpassword(e.target.value)}
               className="w-full h-[50px] bg-transparent outline-none border-[2px] border-[rgba(255,255,255,0.2)] rounded-[40px] text-[16px] text-white px-[20px] pr-[45px]"
             />
             <i className="bx bxs-lock-alt absolute right-[20px] top-1/2 transform -translate-y-1/2"></i>
           </div>
-          <button type="submit" className="w-full h-[45px] bg-white border-none outline-none mt-[20px] rounded-[40px] shadow-[0_0_10px_rgba(0,0,0,0.1)] cursor-pointer text-[16px] text-[#333] font-semibold hover:bg-[#fdc80c] ">
+          <button type="submit" onClick={handleSubmit} className="w-full h-[45px] bg-white border-none outline-none mt-[20px] rounded-[40px] shadow-[0_0_10px_rgba(0,0,0,0.1)] cursor-pointer text-[16px] text-[#333] font-semibold hover:bg-[#fdc80c] ">
             Register
           </button>
-          <button type="button" className="w-full h-[45px] bg-[#fdc80c] border-none outline-none mt-[10px] rounded-[40px] shadow-[0_0_10px_rgba(0,0,0,0.1)] cursor-pointer text-[16px] text-black font-semibold hover:bg-white hover:text-[#333]">
+          <button type="button" onClick={()=>navigate('/user-signup')} className="w-full h-[45px] bg-[#fdc80c] border-none outline-none mt-[10px] rounded-[40px] shadow-[0_0_10px_rgba(0,0,0,0.1)] cursor-pointer text-[16px] text-black font-semibold hover:bg-white hover:text-[#333]">
             Signup as User
           </button>
           <div className="text-center text-[14.5px] my-[20px]">
             <p>
-              Already have an account? <a href="#" className="text-white font-semibold hover:underline">Login</a>
+              Already have an account? <Link to="/captain-login" className="text-white font-semibold hover:underline">Login</Link>
             </p>
           </div>
         </form>
