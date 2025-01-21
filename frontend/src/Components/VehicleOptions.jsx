d85b437243300a4f8295da3cbae47da8b65e3ae6
Ankan Mandal <ankanmandal2001@gmail.com>
2025-01-22 01:19:13 +0530

    User-home almost done

---
+++ frontend/src/Components/VehicleOptions.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Bike from '../assets/Bike.webp'
+import Auto from '../assets/Auto.webp'
+import Cab from '../assets/Cab.webp'
+import { IoPerson } from "react-icons/io5";
+
+
+const VehicleOptions = () => {
+  return (
+    <div className='w-full h-full bg-white flex flex-col items-center  gap-5 '>
+         <div className=' lg:w-[80%] w-[90%] lg:h-[20%] h-[25%] border-2 border-black rounded-2xl flex items-center justify-between px-3 gap-3'>
+               <img src={Bike} alt='bike' className='w-[30%]'/>
+                <div className='w-[50%] h-full flex flex-col justify-center'>
+                    <h1 className='flex items-center gap-2 font-semibold text-base'>Moto <span className='flex items-center'><IoPerson />1</span></h1>
+                    <p className='text-xs font-semibold'>2mins away</p>
+                    <p className='text-xs text-gray-400'>Affordabe Motorcycle Rides</p>
+                </div>
+                <div className='h-full w-[20%] flex items-center px-2 font-bold md:text-sm'>₹65</div>
+        </div>
+        <div className=' lg:w-[80%] w-[90%] lg:h-[20%] h-[25%] border-2 border-black rounded-2xl flex items-center justify-between px-3 gap-3'>
+               <img src={Auto} alt='auto' className='w-[30%]'/>
+                <div className='w-[50%] h-full flex flex-col justify-center'>
+                    <h1 className='flex items-center gap-2 font-semibold text-base'>Auto <span className='flex items-center'><IoPerson />3</span></h1>
+                    <p className='text-xs font-semibold'>2mins away</p>
+                    <p className='text-xs text-gray-400'>Affordabe Motorcycle Rides</p>
+                </div>
+                <div className='h-full w-[20%] flex items-center  font-bold md:text-sm'>₹118.86</div>
+        </div>
+        <div className=' lg:w-[80%] w-[90%] lg:h-[20%] h-[25%] border-2 border-black rounded-2xl flex items-center justify-between px-3 gap-3'>
+               <img src={Cab} alt='cab' className='w-[30%]'/>
+                <div className='w-[50%] h-full flex flex-col justify-center'>
+                    <h1 className='flex items-center gap-2 font-semibold text-base'>Cab <span className='flex items-center'><IoPerson />4</span></h1>
+                    <p className='text-xs font-semibold'>2mins away</p>
+                    <p className='text-xs text-gray-400'>Affordabe Motorcycle Rides</p>
+                </div>
+                <div className='h-full w-[20%] flex items-center font-bold md:text-sm'>₹193.56</div>
+        </div>
+    </div>
+  )
+}
+
+export default VehicleOptions
--- frontend/src/Pages/UserHome.jsx
+++ frontend/src/Pages/UserHome.jsx
@@ -1,16 +1,24 @@
 import React, { useState } from 'react'
 import SuggestedLocations from '../Components/SuggestedLocations';
+import VehicleOptions from '../Components/VehicleOptions';
 
 
 const UserHome = () => {
 
   const [height, setHeight] = useState('0%')
   const [opacity,setOpacity]=useState(0)
+
+  const [location,setLocation]=useState(false);
   
 
   const handleFocus = () => {
     setOpacity(1)
     setHeight('80%');
+  }
+
+  const selectVehiclePanel=(e)=>{
+    e.preventDefault();
+    setLocation(true);
   }
 
   return (
@@ -20,10 +28,12 @@ const UserHome = () => {
           <h2 className='font-bold text-2xl '>Book Your Ride</h2>
           <input className='w-[80%] h-10 rounded-xl px-3 bg-gray-200' placeholder='Pick up' onClick={handleFocus} />
           <input className='w-[80%] h-10 rounded-xl px-3 bg-gray-200' placeholder='Destination' onClick={handleFocus} />
-          <button className='bg-yellow-400 px-10 py-2 font-semibold text-xl rounded-xl'>Confirm</button>
+          <button className='bg-yellow-400 px-10 py-2 font-semibold text-xl rounded-xl' onClick={selectVehiclePanel}>Confirm</button>
         </form>
         <div className='w-full bg-blue-300 transition-all duration-500 ' style={{ height: height,opacity:opacity }}>
-          <SuggestedLocations />
+        {
+          location?<VehicleOptions/>:<SuggestedLocations/>
+        }
         </div>
       </div>
       <div className='md:w-[65%] md:h-screen bg-green-400 w-full h-[70vh] '>
@@ -34,10 +44,12 @@ const UserHome = () => {
           <h2 className='font-bold text-2xl '>Book Your Ride</h2>
           <input className='w-[80%] h-10 rounded-xl px-3 bg-gray-200 text-sm' placeholder='Pick up' onClick={handleFocus} />
           <input className='w-[80%] h-10 rounded-xl px-3 bg-gray-200 text-sm' placeholder='Destination' onClick={handleFocus} />
-          <button className='bg-yellow-400 px-8 py-[8px] font-semibold text-base rounded-xl'>Confirm</button>
+          <button className='bg-yellow-400 px-8 py-[8px] font-semibold text-base rounded-xl' onClick={selectVehiclePanel}>Confirm</button>
         </form>
-        <div className='w-full bg-blue-300 transition-all duration-500 ' style={{ height: height,opacity:opacity }}>
-          <SuggestedLocations />
+        <div className='w-full bg-white transition-all duration-500 pt-5' style={{ height: height,opacity:opacity }}>
+          {
+            location?<VehicleOptions/>:<SuggestedLocations/>
+          }
         </div>
       </div>
     </div>
