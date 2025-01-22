6efd6a3775b1cb2193503f3e3259d8cb2844e35b
Ankan Mandal <ankanmandal2001@gmail.com>
2025-01-22 15:07:14 +0530

    captain home responsive done

--- frontend/src/Components/ConfirmCancelRide.jsx
+++ frontend/src/Components/ConfirmCancelRide.jsx
@@ -3,7 +3,7 @@ import { IoLocationSharp } from "react-icons/io5";
 
 const ConfirmCancelRide = ({handleConfirmRide}) => {
     return (
-        <div className='w-full h-[80%] bg-transparent flex flex-col items-center'>
+        <div className='w-full h-[80%]  bg-transparent flex flex-col items-center'>
             <div className='w-[90%] h-[25%]  flex items-center px-2' >
                 <IoLocationSharp className='w-[5rem] h-[1.3rem] text-green-800' />
                 <p className='text-xs '>44/1, Bharat Apartment, 4C 5th Main Road, Jayanagar, Bangalore 560041, KA, IND </p>
--- frontend/src/Pages/CaptainHome.jsx
+++ frontend/src/Pages/CaptainHome.jsx
@@ -1,39 +1,55 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import ConfirmOtp from '../Components/ConfirmOtp';
 import ConfirmCancelRide from '../Components/ConfirmCancelRide';
 
 
 const CaptainHome = () => {
 
-  const [containerHeight,setContainerHeight]=useState('60%')
-  const [confirmRide,setConfirmRide]=useState(false);
-  const [reachedLoaction, setReachedLocation]=useState(false);
+  const [containerHeight, setContainerHeight] = useState('60%')
+  const [confirmRide, setConfirmRide] = useState(false);
+  const [reachedLoaction, setReachedLocation] = useState(false);
 
-  const handleConfirmRide=()=>{
+  const handleConfirmRide = () => {
     setContainerHeight('40%')
     setConfirmRide(true)
   }
 
-  const handleReachedLocation=()=>{
+  const handleReachedLocation = () => {
     setReachedLocation(true);
   }
 
   return (
-    <div className='w-full h-screen relative'>
-      <div className='w-full h-full bg-green-300'></div>
-      <div className='w-full bg-white absolute bottom-0 flex flex-col items-center justify-center py-1 transition-all duration-500' style={{height:containerHeight}}>
+    <div className='w-full h-screen relative md:flex'>
+      <div className='hidden md:flex flex-col items-center justify-center w-[30rem] h-screen bg-white'>
+        <div className='hidden  bg-white absolute md:flex flex-col items-center py-1 transition-all duration-500' style={{ height: containerHeight }}>
+
+          <h1 className='font-bold text-2xl my-3'>Confirm This Ride To Start</h1>
+          <div className='w-[90%] h-[4rem] bg-yellow-400 flex items-center justify-around rounded-xl'>
+            <img src='https://preview.keenthemes.com/metronic-v4/theme/assets/pages/media/profile/people19.png'
+              className='w-[3rem] md:w-[2.5rem]  border-2 border-black rounded-full pt-1 bg-white'
+            />
+            <h2 className='font-bold'>Ankan Mandal</h2>
+            <h2 className='font-bold'>2.2Km</h2>
+          </div>
+          {
+            (confirmRide === false && reachedLoaction === false) ? <ConfirmCancelRide handleConfirmRide={handleConfirmRide} /> : (confirmRide === true && reachedLoaction === false) ? <button className='w-[80%] h-12 rounded-xl bg-green-500 text-white my-5' onClick={handleReachedLocation}>Reached To Pickup</button> : <ConfirmOtp />
+          }
+        </div>
+      </div>
+      <div className='w-full h-full bg-green-300 md:w-[70%]'></div>
+      <div className='md:hidden w-full bg-white absolute bottom-0 flex flex-col items-center justify-center py-1 transition-all duration-500' style={{ height: containerHeight }}>
         <h1 className='font-bold text-2xl my-3'>Confirm This Ride To Start</h1>
-        <div className='w-[90%] h-[20%] bg-yellow-400 flex items-center justify-around rounded-xl'>
+        <div className='w-[90%] h-[4rem] bg-yellow-400 flex items-center justify-around rounded-xl'>
           <img src='https://preview.keenthemes.com/metronic-v4/theme/assets/pages/media/profile/people19.png'
-            className='w-[12%] border-2 border-black rounded-full pt-1 bg-white'
+            className='w-[3rem]  border-2 border-black rounded-full pt-1 bg-white'
           />
           <h2 className='font-bold'>Ankan Mandal</h2>
           <h2 className='font-bold'>2.2Km</h2>
         </div>
         {
-          (confirmRide===false && reachedLoaction===false)?<ConfirmCancelRide handleConfirmRide={handleConfirmRide}/>:(confirmRide===true && reachedLoaction===false )?<button className='w-[80%] h-12 rounded-xl bg-green-500 text-white my-5' onClick={handleReachedLocation}>Reached To Pickup</button>:<ConfirmOtp/>
+          (confirmRide === false && reachedLoaction === false) ? <ConfirmCancelRide handleConfirmRide={handleConfirmRide} /> : (confirmRide === true && reachedLoaction === false) ? <button className='w-[80%] h-12 rounded-xl bg-green-500 text-white my-5' onClick={handleReachedLocation}>Reached To Pickup</button> : <ConfirmOtp />
         }
-        
+
       </div>
     </div>
   )
